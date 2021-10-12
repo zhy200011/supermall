58d31c7d42c8060393457110fe5205c29da822de
zhy200011 <1832659877@qq.com>
2021-10-12 20:14:47 +0800

    Signed-off-by: zhy200011 <1832659877@qq.com>

---
+++ src/components/common/MainSwiper/MainSwiper.vue
@@ -0,0 +1,41 @@
+<template>
+
+		 <div class="swiper-container " ref="swipercontainer">
+		    <div class="swiper-wrapper">
+				<slot></slot>
+			</div>
+			<!-- 分页器 -->
+			<div class="swiper-pagination"></div>
+		  </div>
+
+
+</template>
+
+<script>
+	import Swiper from "swiper/swiper-bundle.min.js"
+	import 'swiper/swiper-bundle.css';
+	import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+	
+	SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+	export default{
+		mounted() {
+			let mainSwiper=new Swiper(this.$refs.swipercontainer,{
+				loop:true,//循环
+				centeredSlides:true,
+				pagination:{
+					el:'.swiper-pagination',
+					clickable:true
+				},
+				autoplay:{
+					delay:2000,
+					disableOnInteraction:false
+				}
+			})
+		},
+	}
+</script>
+		
+<style scoped>
+	
+	
+</style>
